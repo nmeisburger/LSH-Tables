0ae3c17649bbba01dd56d26c0affa2dd4c101d3b
nmeisburger0 <ncm5@rice.edu>
2020-05-13 12:53:46 -0700

    comments

--- config.h
+++ config.h
@@ -1,3 +1,6 @@
+// The number of hash tables.
 #define NUM_TABLES 4
+// The size of the reservoirs in the hash tables.
 #define RESERVOIR_SIZE 7
+// The log base 2 of the number of reservoirs in each hash table.
 #define RANGE_POW 2
